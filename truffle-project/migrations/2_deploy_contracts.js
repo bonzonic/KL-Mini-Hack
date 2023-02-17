f2d4f777ff87953a3cd2b20b62d400ea208f3029
Wong Nicholas <wongyiznicholas@gmail.com>
2023-02-17 17:17:44 +0800

    cabbage coin integration

--- truffle-project/migrations/2_deploy_contracts.js
+++ truffle-project/migrations/2_deploy_contracts.js
@@ -2,16 +2,22 @@ const fs = require('fs');
 
 const VotingEvent = artifacts.require("VotingEvent");
 const Hasher = artifacts.require("Hasher");
+const simpleContract = artifacts.require("CabbageCoin");
 
-module.exports = function (deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
   // const owners = accounts.slice(0, 1);
 
   const hashser = deployer.deploy(Hasher);
 
-  deployer.deploy(VotingEvent, accounts[0], Hasher.address);
+  // voting
+  await deployer.deploy(VotingEvent, accounts[0], Hasher.address);
 
+  // cabbage coin
+  await deployer.deploy(simpleContract);
+  await simpleContract.deployed();
   const data = {
-    VotingEventAddr: VotingEvent.address
+    VotingEventAddr: VotingEvent.address,
+    CabbageCoinAddr: simpleContract.address
   }
 
   fs.writeFileSync('contractAddr.json', JSON.stringify(data), (err) => {
