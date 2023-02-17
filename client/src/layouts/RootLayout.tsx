e705c4fccdccb0a433104b9bc654d6acd1aa2e0a
Wong Nicholas <wongyiznicholas@gmail.com>
2023-02-17 21:18:15 +0800

    rootlayout change

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -40,64 +40,47 @@ const RootLayout = () => {
         }
       });
   }
-  const handleLog = () => {
-    if (loggedIn) {
-      Sweetalert2.fire({
-        icon: "info",
-        iconColor: "teal",
-        title: "Logout?",
-        text: "You are already logged in",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "logout",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          dispatch(logout());
+    const handleLog = () => {
+        if (loggedIn) {
+            Sweetalert2.fire({
+                icon: "info",
+                iconColor: "teal",
+                title: "Logout?",
+                text: "You are already logged in",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "logout",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    dispatch(logout());
+                }
+            });
         }
-      });
     }
-
     return (
-      <div>
-        <header className=" nav-container text-black">
-          <nav className="flex justify-between">
-            <div className="flex flex-row items-center">
-              <NavLink
-                className="flex items-center text-black my-5 mx-3 transition duration-500 hover:scale-110"
-                to="/"
-              >
-                <img className="logo w-10 h-10" src={logo} />
-                <h1 className="ml-2">ZKP Voting System</h1>
-              </NavLink>
-            </div>
-            <div className="flex flex-row items-center">
-              {loggedIn ? (
-                <span className="nav-list-item font-bold" onClick={handleLog}>
-                  Log In
-                </span>
-              ) : (
-                <NavLink to="login" className="nav-list-item">
-                  Log In
-                </NavLink>
-              )}
-              <NavLink
-                to="profile"
-                className="rounded-full bg-gray-100 p-1 mr-2 shadow-lg border-2 border-solid border-teal-600 transition duration-500 hover:scale-110"
-              >
-                <img
-                  src={profileLogo}
-                  className="w-6 h-6 !fill-white profile-icon-nav"
-                  alt="Profile Logo"
-                />
-              </NavLink>
-            </div>
-          </nav>
-        </header>
+        <div>
+            <header className=' nav-container text-black'>
+                <nav className="flex justify-between">
+                    <div className="flex flex-row items-center">
+                        <NavLink className="flex items-center text-black my-5 mx-3 transition duration-500 hover:scale-110" to="/">
+                            <img className="logo w-10 h-10" src={logo} />
+                            <h1 className="ml-2">ZKP Voting System</h1>
+                        </NavLink>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        {loggedIn ? (<span className="nav-list-item font-bold" onClick={handleLog}>Log In</span>) :
+                        (<NavLink to="login" className="nav-list-item">Log In</NavLink>)}
+                        <NavLink to="profile" className="rounded-full bg-gray-100 p-1 mr-2 shadow-lg border-2 border-solid border-teal-600 transition duration-500 hover:scale-110">
+                            <img src={profileLogo} className="w-6 h-6 !fill-white profile-icon-nav" alt="Profile Logo" />
+                        </NavLink>
+                    </div>
+                </nav>
+            </header>
 
-        <Outlet context={[coins, setCoins]} />
-      </div>
-    );
-  };
-};
-export default RootLayout;
+            <Outlet context={[coins, setCoins]} />
+        </div>
+    )
+}
+
+export default RootLayout
