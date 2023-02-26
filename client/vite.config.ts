d85dc5fdc888f819cd71b15f4c216de2a1d83094
Wong Nicholas <76901263+bonzonic@users.noreply.github.com>
2023-02-26 15:36:41 +0800

    removed the base

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/KL-Mini-Hack/',
-  plugins: [react()]
+  plugins: [react(), 
+    define({
+      'process.env': {
+        BASE_URL: JSON.stringify('/KL-Mini-Hack/')
+      }
+    })]
 })
