bc68d3a46f71455afc56ed5d2a0f1a0b56dd8063
Wong Nicholas <76901263+bonzonic@users.noreply.github.com>
2023-02-26 15:37:47 +0800

    import define

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { define } from '@vitejs/plugin-define';
 
 // https://vitejs.dev/config/
 export default defineConfig({
