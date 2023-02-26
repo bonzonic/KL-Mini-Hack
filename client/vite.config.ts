169f24e9d85ce3ce5bc8fc967029f3cd64052f01
Nicholas <wongyiznicholas@gmail.com>
2023-02-26 16:09:27 +0800

    updated base again

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { define } from '@vitejs/plugin-define';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/KL-Mini-Hack/',
-  plugins: [react(), 
-    define({
-      'process.env': {
-        BASE_URL: JSON.stringify('/KL-Mini-Hack/')
-      }
-    })]
+  plugins: [react()]
 })
